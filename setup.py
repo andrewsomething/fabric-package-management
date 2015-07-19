86f4ee25c479ad0225529cd2c6f93f13aabf8df8
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2015-07-18 17:21:48 -0700

    Add package info to setup.py for 0.1 release.

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
-
 from setuptools import setup, find_packages
-import sys, subprocess
+
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(
-    name = 'fabric-package-management',
-    version = '0.1',
+    name='fabric-package-management',
+    version='0.1',
     description='A collection of fabric tasks for package management',
+    long_description=long_description,
+    url='https://github.com/andrewsomething/fabric-package-management',
+    download_url='https://github.com/andrewsomething/fabric-package-management/releases',
+    license='MIT License (Expat)',
 
     author='Andrew Starr-Bochicchio',
     author_email='a.starr.b@gmail.com',
 
-    packages = find_packages(),
-    install_requires = ['fabric']
+    packages=find_packages(),
+    install_requires=['fabric']
 )
